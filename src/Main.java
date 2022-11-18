68ade9e4ab9a48a938fe6846ed571d99c5aca28e
k88936 <1440978305@qq.com>
2022-11-19 01:12:54 +0800

    restart with tears

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,30 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         String code;
         compiler compiler = new compiler();
 
-        code="add(1 2)";
+        code="add(1 add(2) 2)";
 
         System.out.println(code);
-        ArrayList<Object> tokens = compiler.tokenizer( code);
+        tree tokens = compiler.tokenizer( code);
+
+        System.out.println(tokens.toString());
 
 
-
-        System.out.println(tokens);
-        HashMap<String, Object> ast = compiler.parser(tokens);
-        System.out.println(ast);
-        HashMap<String, Object> newAst = compiler.transformer(ast);
-        System.out.println(newAst);
-
-
-
-
-        ArrayList<HashMap<String, Object>> array = new ArrayList<>();
-        compiler.argumentDistribute(newAst,array,0);
-        System.out.println(array);
-        String outPut = compiler.codeGenerator(array);
-        System.out.println(outPut);
+//        System.out.println(tokens);
+//        HashMap<String, Object> ast = compiler.parser(tokens);
+//        System.out.println(ast);
+//        HashMap<String, Object> newAst = compiler.transformer(ast);
+//        System.out.println(newAst);
+//
+//
+//
+//
+//        ArrayList<HashMap<String, Object>> array = new ArrayList<>();
+//        compiler.argumentDistribute(newAst,array,0);
+//        System.out.println(array);
+//        String outPut = compiler.codeGenerator(array);
+//        System.out.println(outPut);
     }
 }
 
